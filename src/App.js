fbbc5707ddc930de0c7442f5a6ef53fedd0cb190
Pradeep A <sharmapradeep872@gmail.com>
2021-12-02 23:52:49 +0530

    Added child components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Navbar from "./Views/Navbar/Navbar";
+import Sidebar from "./Views/Sidebar/Sidebar";
+import MainBody from "./Views/MainContent/MainBody";
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial Setup</h1>
+      <Navbar />
+      <div className="gridContainer">
+        <Sidebar />
+        <MainBody />
+      </div>
     </div>
   );
 }
